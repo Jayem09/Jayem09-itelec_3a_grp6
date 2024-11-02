d747a84c07ed3cc1b395ff57d0a9dd7c97dd1d5e
Jayem09 <johndinglasan12@gmail.com>
2024-11-02 05:12:53 +0100

    navbar childrens fixs,  rooms and hero test

--- rental-management/src/pages/Dashboard/rooms.js
+++ rental-management/src/pages/Dashboard/rooms.js
@@ -1,9 +1,7 @@
-import React from 'react'
-
+import React from "react";
+import Hero from "../../components/Hero";
 function rooms() {
-  return (
-    <div>rooms</div>
-  )
+  return <Hero hero="roomsHero" />;
 }
 
-export default rooms
+export default rooms;
