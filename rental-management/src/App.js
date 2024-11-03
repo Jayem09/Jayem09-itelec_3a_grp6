aac65b06362af4b53a4fb54a75596aa9f678b847
Jayem09 <johndinglasan12@gmail.com>
2024-11-03 16:00:23 +0100

    Error, app, banner, home , hero pages  added

--- rental-management/src/App.js
+++ rental-management/src/App.js
@@ -17,7 +17,7 @@ function App() {
         {/* <Route path='/' element={<Login />} /> */}
         {/* <Route path='/login' element={<Login />} /> */}
         {/* <Route path='/signup' element={<Signup />} /> */}
-        <Route path="/dashboard" element={<Dashboard />} />
+        {/* <Route path="/dashboard" element={<Dashboard />} />  */}
         <Route path="/" element={<Home />} />
         <Route path="/error" element={<Error />} />
         <Route path="/rooms" element={<Rooms />} />
--- rental-management/src/pages/Dashboard/error.js
+++ rental-management/src/pages/Dashboard/error.js
@@ -1,9 +1,5 @@
 import React from "react";
 import Hero from "../../components/Hero";
 export default function error() {
-  return (
-    <div>
-      <Hero />
-    </div>
-  );
+  return <Hero />;
 }
